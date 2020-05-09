a6944295b2c9c6f26d926b66e79273d5f36522c5
Samet Şahingöz <sametaz57@gmail.com>
2020-05-09 19:54:03 +0000

    Initial commit

---
+++ donguler.java
@@ -0,0 +1,26 @@
+class dongu {
+  public static void main(String[] args) {
+   
+   for(int i = 1; i<=10; i++ )
+   {
+     System.out.println(i);
+   }
+
+   int sayi = 10;
+   while(sayi <=100)
+   {
+     System.out.println(sayi);
+     sayi += 10;
+   }
+  
+  
+   int sayi2 =10;
+   
+   do
+   {
+     System.out.println(sayi2);
+      sayi2 +=10;
+   }
+   while(sayi2>= 100);
+  }
+}
---
+++ sartBloklari.java
@@ -0,0 +1,15 @@
+class degisken {
+  public static void main(String[] args) {
+   int not;
+   not =5;
+   if(not>=50)
+   {
+     System.out.println("samet geçti");
+   }
+   else if(not <50)
+   {
+     System.out.println("Kaldı");
+   }
+    
+  }
+}
